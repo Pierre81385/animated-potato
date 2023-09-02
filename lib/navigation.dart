787e5ae38d80242e6b3159b59c1ba63a9a866ccb
Peter Bishop <pjb.den@gmail.com>
2023-09-01 19:06:12 -0600

    resolved minor issue with selected state on bottom nav bar

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -22,14 +22,15 @@ class _NavComponentState extends State<NavComponent> {
   late double width;
   late String device;
   late void Function(int) _onSelect;
-  int _selectedIndex = 1; //New
+  int _selectedIndex = 1;
+  int _activeNav = 1; //New
 
-  // void _onItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //     _onSelect(index);
-  //   });
-  // }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _onSelect(index);
+  }
 
   @override
   void initState() {
@@ -46,7 +47,7 @@ class _NavComponentState extends State<NavComponent> {
     return device == "mobile"
         ? BottomNavigationBar(
             currentIndex: _selectedIndex, //New
-            onTap: _onSelect,
+            onTap: _onItemTapped,
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                 icon: Icon(Icons.account_tree_sharp),
@@ -67,18 +68,51 @@ class _NavComponentState extends State<NavComponent> {
               TextButton(
                   onPressed: () {
                     _onSelect(0);
+                    setState(() {
+                      _activeNav = 0;
+                    });
                   },
-                  child: Text("Projects")),
+                  child: Text(
+                    _activeNav == 0
+                        ? "projects".toUpperCase()
+                        : "projects".toLowerCase(),
+                    style: TextStyle(
+                        fontWeight: _activeNav == 0
+                            ? FontWeight.bold
+                            : FontWeight.normal),
+                  )),
               TextButton(
                   onPressed: () {
                     _onSelect(1);
+                    setState(() {
+                      _activeNav = 1;
+                    });
                   },
-                  child: Text("About")),
+                  child: Text(
+                    _activeNav == 1
+                        ? "about".toUpperCase()
+                        : "about".toLowerCase(),
+                    style: TextStyle(
+                        fontWeight: _activeNav == 1
+                            ? FontWeight.bold
+                            : FontWeight.normal),
+                  )),
               TextButton(
                   onPressed: () {
                     _onSelect(2);
+                    setState(() {
+                      _activeNav = 2;
+                    });
                   },
-                  child: Text("Experience")),
+                  child: Text(
+                    _activeNav == 2
+                        ? "experience".toUpperCase()
+                        : "experience".toLowerCase(),
+                    style: TextStyle(
+                        fontWeight: _activeNav == 2
+                            ? FontWeight.bold
+                            : FontWeight.normal),
+                  )),
             ],
           );
   }
